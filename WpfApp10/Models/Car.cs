d13e1b940f4939f5037ee709c87417e17ca394f9
heliconia8 <d.samokvasova@gmail.com>
2023-05-11 11:16:07 +0400

    Добавьте файлы проекта.

---
+++ WpfApp10/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp10.Models
+{
+   public class Car
+   {
+        public string Marka { get; set; }
+        public string Color { get; set; }
+   }
+}
---
+++ WpfApp10/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp10.Models;
+
+namespace WpfApp10.ViewModel
+{
+    internal partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty] private Car selectedCar;
+
+        [RelayCommand]
+        private void GenerateRandomCar()
+        {
+            string[] marks = { "Kia", "Lada" };
+            string[] colors = { "red", "green" };
+
+            Car car = new Car()
+            {
+                Color = colors[new Random().Next(0, colors.Length)],
+                Marka = marks[new Random().Next(0, marks.Length)]
+            };
+            SelectedCar = car;
+        }
+    }
+}
